9bf54336c978f5e91fd4fcf8fbd1aba680e463fa
Robert Henderson <robert@nterpryz.com>
2018-02-28 12:14:11 +0000

    added initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+});
+
+app.use(express.static('client/src'))
+
+const server = app.listen(3000, function(){
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
